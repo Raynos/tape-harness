5f0ad43e5a53ae7bcea37d5fdfb334342535914e
Raynos <raynos2@gmail.com>
2019-03-11 16:11:11 +0100

    fix test coverage

--- test/index.js
+++ test/index.js
@@ -13,7 +13,7 @@ function MyTestCluster(opts) {
 
     var self = this;
 
-    self.port = opts.port;
+    self.port = opts.port || 0;
     self.server = http.createServer();
 
     self.server.on('request', onRequest);
@@ -26,7 +26,10 @@ function MyTestCluster(opts) {
 MyTestCluster.prototype.bootstrap = function bootstrap(cb) {
     var self = this;
 
-    self.server.once('listening', cb);
+    self.server.once('listening', function onListen() {
+        self.port = self.server.address().port;
+        cb();
+    });
     self.server.listen(self.port);
 };
 
@@ -50,5 +53,86 @@ MyTestCluster.test('a test', {
         assert.equal(resp.body, '/foo');
 
         assert.end();
+    });
+});
+
+MyTestCluster.test('no options', function t(cluster, assert) {
+    request({
+        url: 'http://localhost:' + cluster.port + '/foo'
+    }, function onResponse(err, resp, body) {
+        assert.ifError(err);
+
+        assert.equal(resp.statusCode, 200);
+        assert.equal(resp.body, '/foo');
+
+        assert.end();
+    });
+});
+
+MyTestCluster.test('no function name');
+
+tape('handles bootstrap error', function t(assert) {
+    function TestClass() {}
+
+    TestClass.prototype.bootstrap = function b(cb) {
+        cb(new Error('it failed'));
+    };
+    TestClass.prototype.close = function c(cb) {
+        cb();
+    };
+
+    var myTest = tapeCluster(function testFn(name, fn) {
+        var hasError = false;
+        assert.equal(name, 'a name');
+        fn({
+            end: function end() {
+                assert.ok(hasError);
+
+                assert.end();
+            },
+            ifError: function ifError(err) {
+                hasError = true;
+                assert.ok(err);
+                assert.equal(err.message, 'it failed');
+            }
+        });
+    }, TestClass);
+
+    myTest('a name', function _(assertLike) {
+        assert.fail('should not reach here');
+        assertLike.end();
+    });
+});
+
+tape('handles close error', function t(assert) {
+    function TestClass() {}
+
+    TestClass.prototype.bootstrap = function b(cb) {
+        cb();
+    };
+    TestClass.prototype.close = function c(cb) {
+        cb(new Error('it failed'));
+    };
+
+    var myTest = tapeCluster(function testFn(name, fn) {
+        var hasError = false;
+        assert.equal(name, 'a name');
+        fn({
+            end: function end() {
+                assert.ok(hasError);
+
+                assert.end();
+            },
+            ifError: function ifError(err) {
+                hasError = true;
+                assert.ok(err);
+                assert.equal(err.message, 'it failed');
+            }
+        });
+    }, TestClass);
+
+    myTest('a name', function _(cluster, assertLike) {
+        assert.ok(cluster);
+        assertLike.end();
     });
 });
